5a09eb0d66b69d5677185108d417e50fc03a0424
Jessica Perina <jperina@gmail.com>
2019-04-13 16:04:45 -0700

    more redux devtools issues

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import App from './components/App'
 import reducers from './reducers'
