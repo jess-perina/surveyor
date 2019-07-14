a8f194db61c444c786c06845617a83dfb9046bdc
Jessica Perina <jperina@gmail.com>
2019-07-14 16:44:23 -0700

    refactor(survey): move form fields into own file and import into components to dry up code

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,17 +1,40 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import formFields from './formFields'
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = formFields.map(({ label, name }) => {
+      return (
+        <div key={name}>
+          <label>{label}</label>
+          <div>
+            {formValues[name]}
+          </div>
+        </div>
+      )
+    }
+  )
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button
-        className="yellow darken-3 btn-flat"
-        onClick={onCancel}
-      >
+      {reviewFields}
+      <button className="yellow darken-3 btn-flat white-text" onClick={onCancel}>
         Back
+      </button>
+      <button className="green btn-flat white-text right">
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   )
 }
 
-export default SurveyFormReview
+const mapStateToProps = (state) => {
+  console.log(state) 
+  return {
+    formValues: state.form.surveyForm.values
+  }
+}
+
+export default connect(mapStateToProps, {})(SurveyFormReview)
