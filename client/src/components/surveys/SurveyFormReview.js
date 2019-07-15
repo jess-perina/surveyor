37d9cf99079dad7fd53dbd84d3f831188bba2cc6
Jessica Perina <jperina@gmail.com>
2019-07-15 14:44:43 -0700

    feat(redux): add action creator for submitting surveys with programatic redirect

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import formFields from './formFields'
+import * as actions from '../../actions'
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = formFields.map(({ label, name }) => {
       return (
         <div key={name}>
@@ -19,10 +21,12 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 btn-flat white-text" onClick={onCancel}>
+      <button className="yellow darken-3 btn-flat white-text"
+        onClick={onCancel}>
         Back
       </button>
-      <button className="green btn-flat white-text right">
+      <button className="green btn-flat white-text right"
+        onClick={() => submitSurvey(formValues, history)}>
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -31,10 +35,9 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state) 
   return {
     formValues: state.form.surveyForm.values
   }
 }
 
-export default connect(mapStateToProps, {})(SurveyFormReview)
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview))
