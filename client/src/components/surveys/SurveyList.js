b215efff5389c3d75d789ce7794db7a5aae6ff7b
Jessica Perina <jperina@gmail.com>
2019-07-16 13:46:14 -0700

    feat(survey): add delete survey to component and hook to redux

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -1,30 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchSurveys } from '../../actions'
+import { fetchSurveys, deleteSurvey } from '../../actions'
 
 class SurveyList extends Component {
   componentDidMount() {
     this.props.fetchSurveys()
   }
 
+  surveyDelete(id) {
+    this.props.deleteSurvey(id)
+  }
+
   renderSurveys() {
-    return this.props.surveys.reverse().map(survey => {
-      return (
-        <div key={survey._id} class="card blue-grey lighten-1 white-text">
-          <div class="card-content">
-            <span class="card-title">{survey.title}</span>
-            <p>{survey.body}</p>
-            <p className="right">
-              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
-            </p>
+    console.log(this.props.surveys)
+    return this.props.surveys.sort((a, b) => new Date(b.dateSent) - new Date(a.dateSent))
+      .map(survey => {
+        return (
+          <div key={survey._id} className="card blue-grey lighten-1 white-text">
+            <div className="card-content">
+              <span className="card-title">{survey.title}</span>
+              <p>{survey.body}</p>
+              <p className="right">
+                Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+              </p>
+            </div>
+            <div className="card-action" style={{padding: '20px 24px'}}>
+              <a href="!#">YES: {survey.yes}</a>
+              <a href="!#">NO: {survey.no}</a>
+              <button className="right btn-flat red white-text"
+                onClick={() => {this.surveyDelete(survey._id)}}>
+                Delete
+              </button>
+            </div>
           </div>
-          <div class="card-action">
-            <a href="!#">YES: {survey.yes}</a>
-            <a href="!#">NO: {survey.no}</a>
-          </div>
-        </div>
-      )
-    })
+        )
+      })
   }
 
   render() {
@@ -40,4 +50,4 @@ const mapStateToProps = ({ surveys }) => {
   return { surveys }
 }
 
-export default connect(mapStateToProps, { fetchSurveys })(SurveyList)
+export default connect(mapStateToProps, { fetchSurveys, deleteSurvey })(SurveyList)
